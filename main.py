01af48494acd96a9a1431e00aa3bb5bf6ba1f4c1
Kirill <Kirill.Teplekov@yandex.ru>
2019-03-13 21:02:28 +0300

    Add new decorator and functions for /login, /registration, /logout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+from forms import LoginForm, RegistrationForm
+from project_db import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -16,7 +19,31 @@ def index():
 
 @app.route('/login')
 def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_name = form.username.data
+        hash = generate_password_hash(form.password.data)
+        user_model = Reader()
+        exists = user_model.exists(user_name, hash)
+        if exists[0]:
+            session['username'] = user_name
+            session['user_id'] = exists[1]
+            return redirect('/index')
+        else:
+            return redirect('/login')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/registration')
+def registration():
     pass
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', 0)
+    session.pop('user_id', 0)
+    return redirect('/login')
 
 
 @app.route('/book_order')
