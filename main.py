bbc05cf15cb411b81118c9d8a882d0b7803e2a67
Kirill <Kirill.Teplekov@yandex.ru>
2019-03-13 17:54:40 +0300

    Add body of project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,3 +7,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    pass
+
+
+@app.route('/login')
+def login():
+    pass
+
+
+@app.route('/book_order')
+def book_order():
+    pass
+
+
+@app.route('/<username>')
+def user_page(username):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
