39adfb2ed53bf779308aaec3897c706a26edb3f3
Kirill <Kirill.Teplekov@yandex.ru>
2019-03-17 22:01:50 +0300

    Fix some

--- forms.py
+++ forms.py
@@ -12,8 +12,10 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     town = StringField('Город(не обязательно к заполнению)')
-    image = FileField('Загрузите изображение')
+    image = FileField('Загрузите изображение', validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
