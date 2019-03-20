701f22d0fe4b5a1e66f75ad0538be842029707ed
Kirill <Kirill.Teplekov@yandex.ru>
2019-03-21 00:35:55 +0300

    Fix some errors and api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import reqparse, abort, Api, Resource
 from forms import LoginForm, RegistrationForm, AddBook
-from werkzeug.utils import secure_filename
+from requests import get, delete
 import os
 from project_db import *
 
@@ -12,23 +12,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 api = Api(app)
-UPLOAD_FOLDER = '/static/'
+UPLOAD_FOLDER = 'static/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # Main page
 @app.route('/')
 @app.route('/index')
 def index():
     # Create list with last 5 books
-    new_books = Book.query.all()[-6:]
-    # Authorization check
-    if 'username' in session:
-        # If user authorized show user's name and image
-        username = session['username']
-        image = session['image']
-        return render_template('index.html', title='Библиотека', username=username,
-                               image=image, new_book=new_books)
-    return render_template('index.html', title='Библиотека', new_books=new_books)
+    new_books = Book.query.all()[-3:]
+    return render_template('index.html', title='Библиотека', new_books=new_books, image='static\lib_photo.png')
 
 
 # Sign-in page
@@ -63,24 +57,33 @@ def registration():
     form = RegistrationForm()
 
     if form.validate_on_submit():
+        # Check for the existence of the user in db
         user = Reader.query.filter_by(username=form.username.data).first()
         if not user:
             if form.image:
-                image = form.image.data.read()
-                image.save(os.path.join(app.config['UPLOAD_FOLDER'], form.username.data))
+                try:
+                    image_name = os.path.join(app.config['UPLOAD_FOLDER'] + form.username.data + '.png')
+                    with open(image_name, 'wb') as file:
+                        file.write(form.image.data.read())
+                        file.close()
+                except Exception as e:
+                    return render_template('error_page.html', title='Ошибка', error=e)
             else:
-                image = 'static/user.png'
+                image_name = 'static/user.png'
+
             # Create new user
             new_user = Reader(username=form.username.data, name=form.name.data,
                               surname=form.surname.data, email=form.email.data,
-                              town=form.town.data, image=image,
-                              hash=form.password.data)
+                              town=form.town.data, image=image_name,
+                              hash=form.password.data, admin=False)
             db.session.add(new_user)
             db.session.commit()
             session['username'] = new_user.username
             session['user_id'] = new_user.id
             session['image'] = new_user.image
             return redirect('/index')
+        else:
+            return render_template('error_page.html', title='Ошибка', error='Такой пользователь уже существует')
     return render_template('registration.html', title='Регистрация', form=form)
 
 
@@ -88,6 +91,7 @@ def registration():
 def logout():
     session.pop('username', 0)
     session.pop('user_id', 0)
+    session['admin'] = False
     return redirect('/login')
 
 
@@ -99,31 +103,29 @@ def add_book():
     if form.validate_on_submit():
         book = Book.query.filter_by(name=form.name.data).first()
         if not book:
-            my_file = secure_filename(form.icon.data.file.filename)
-            my_file.save(os.path.join(app.config['UPLOAD_FOLDER'], form.name.data))
-            icon.save(os.path.join(app.config['UPLOAD_FOLDER'], form.name.data))
+            try:
+                icon_name = os.path.join(app.config['UPLOAD_FOLDER'] + form.name.data + '.png')
+                with open(icon_name, 'wb') as icon:
+                    icon.write(form.icon.data.read())
+                    icon.close()
+            except Exception as e:
+                return render_template('error_page.html', title='Ошибка', error=e)
+
+            # Add new book in db
             new_book = Book(name=form.name.data, author=form.author.data,
                             count_all_books=form.count_all_books.data,
-                            count_book_in_lib=form.count_all_books.data, icon=icon,
+                            count_book_in_lib=form.count_all_books.data, icon=icon_name,
                             description=form.description.data)
-            new_book_primary = PrimaryBook()
-            new_book.PrimaryBooks.append(new_book_primary)
             db.session.add(new_book)
             db.session.commit()
             return redirect('/success')
-    username = session['username']
-    image = session['image']
-    return render_template('add_book.html', title='Добавить книгу', form=form, username=username, image=image)
+        else:
+            return render_template('error_page.html', title='Ошибка', error='Книга уже была создана')
+    return render_template('add_book.html', title='Добавить книгу', form=form)
 
 
 @app.route('/success')
 def success():
-    if 'username' in session:
-        # If user authorized show user's name and image
-        username = session['username']
-        image = session['image']
-        return render_template('success.html', title='Успех', username=username,
-                               image=image)
     return render_template('success.html', title='Успех')
 
 
@@ -131,13 +133,6 @@ def success():
 @app.route('/book_order')
 def book_order():
     books = Book.query.all()
-    # Authorization check
-    if 'username' in session:
-        # If user authorized show user's name and image
-        username = session['username']
-        image = session['image']
-        return render_template('book_order.html', title='Список книг', username=username,
-                               image=image, books=books)
     return render_template('book_order.html', title='Список книг', books=books)
 
 
@@ -145,27 +140,20 @@ def book_order():
 @app.route('/users/<user>')
 def user_page(user):
     user = Reader.query.filter_by(username=user).first()
-    # Authorization check
-    if 'username' in session:
-        # If user authorized show user's name and image
-        username = session['username']
-        image = session['image']
-        return render_template('user_page.html', title=user.name, username=username,
-                               image=image, user=user)
-    return render_template('user_page.html', title=user.name, user=user)
+    rows = Associative.query.filter_by(reader_id=user.id)
+    book_order = []
+    for row in rows:
+        book_order.append(Book.query.filter_by(associative=row.id).first())
+    if user:
+        return render_template('user_page.html', title=user.usernamename, user=user, book_order=book_order, delete=delete)
+    else:
+        return render_template('error_page.html', title='Ошибка', error='Такого пользователя нет')
 
 
 # Book's page
 @app.route('/books/<book>')
 def book_page(book):
     book = Book.query.filter_by(name=book).first()
-    # Authorization check
-    if 'username' in session:
-        # If user authorized show user's name and image
-        username = session['username']
-        image = session['image']
-        return render_template('book_page.html', title=book.name, username=username,
-                               image=image, book=book)
     return render_template('book_page.html', title=book.name, book=book)
 
 
@@ -174,24 +162,32 @@ def book_page(book):
 ###########
 
 class BookApi(Resource):
-    def get_to_user(self, user_id, book_id):
-        book = PrimaryBook.query.filter_by(id=book_id)
+    # Add get to User
+    def get(self, book_id, user_id):
+        associative = Associative()
+        book = Book.query.filter_by(id=book_id)
+        book.Associatives.append(associative)
         reader = Reader.query.filter_by(id=user_id)
-        reader.PrimaryBooks.append(book)
+        reader.Associatives.append(associative)
+        book.count_book_in_lib -= 1
+        db.session.add(associative)
         db.session.commit()
 
-    def get(self, book_id):
-        return Book.query.filter_by(id=book_id)
-
-    def delete(self, book_id):
-        Book.query.filter_by(id=book_id).delete()
+    # Delete book from library
+    def delete(self, book_id, user_id):
+        Book.query.filter_by(id=book_id).first().delete()
 
 
-class ReadersApi(Resource):
-    def delete_book(self, reader, book_id):
-        reader = Reader.query.filter_by(username=reader).first()
-        reader.PrimaryBooks.delete()
+class ReaderApi(Resource):
+    # Delete book from user's order
+    def get(self, user_id, book_id):
+        Associative.query.filter_by(reader_id=user_id, book_id=book_id).delete()
+        Book.query.filter_by(id=book_id).first().count_in_lib += 1
 
+
+api.add_resource(BookApi, '/books/<int:book_id>/<int:user_id>')
+api.add_resource(ReaderApi, '/users/<int:user_id>/<int:book_id>')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(port=8080, host='127.0.0.1')
